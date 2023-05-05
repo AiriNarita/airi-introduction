b5f07abe40bc62603dc9c38be0ad4094e4c0745f
AiriNarita <airiswim.kitty@gmail.com>
2023-05-05 11:54:22 +0900

    feat: personal/skill card text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { motion } from "framer-motion";
-import { FaTwitter, FaGithub, FaBook, FaRegSmileBeam } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaBook, FaRegSmileBeam, FaHeart } from "react-icons/fa";
 import { SiZenn } from "react-icons/si";
 import { GiHumanEar } from "react-icons/gi";
 
@@ -95,22 +95,12 @@ export default function Home() {
                 whileTap={{ scale: 0.9 }}
                 className={styles.personalIcon}
               >
-                <GiHumanEar size={"3rem"} />
+                <FaHeart size={"3rem"} />
               </motion.div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus nobis voluptas dolorem, ut odit fuga sapiente
-                tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati?
+              <strong>興味と好奇心の塊です</strong><br/>
+              興味関心を持ったものは、やらない以外にない！そしてやるときめたら最後までとことん知りたい！経験したい！という性格です。
+              ページトップにも書いた通り、不可能なことはないと考えています。<strong>人間は努力次第で自分の想像を超えられるし、その先にまた新しい可能性が待っている</strong>と思っています。
               </p>
             </motion.div>
             <motion.div variants={item} className={styles.personalCard}>
@@ -122,20 +112,17 @@ export default function Home() {
                 <FaBook size={"3rem"} />
               </motion.div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus nobis voluptas dolorem, ut odit fuga sapiente
-                tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati?
+                <strong>勉強すること、新しい刺激が好きです</strong><br/>
+                私が勉強で心掛けている3点は...<br/>
+                【 <strong>1. 絶対に１日１つアウトプット</strong>】<br/>
+                いいインプットがあってこそアウトプットができるし、アウトプットできてこそ定着!<br/>
+                【 <strong>2. 理論の理解</strong>】<br/>
+                &quot;<strong>なぜ、何のために??</strong>&quot;
+                なんでこの言語が生まれたのか？<strong>意味や経緯、歴史</strong>を探っていくことで適切なコードをかけるようになると思うし、応用できる知識にするためにも、必須だと思っています。他の言語学ぶ時も入ってきやすいです。<br/>
+                【 <strong>3. なんでもやってみる！</strong>】<br/>
+                失敗してこそ学びが大きい!<strong>やってみてこそわかることの情報量が多い！</strong>やってみない理由なんてある？❤︎
               </p>
+
             </motion.div>
             <motion.div variants={item} className={styles.personalCard}>
               <motion.div
@@ -146,19 +133,9 @@ export default function Home() {
                 <FaRegSmileBeam size={"3rem"} />
               </motion.div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus nobis voluptas dolorem, ut odit fuga sapiente
-                tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati?
+              <strong>コミュニケーションが好きです</strong><br/>
+              前職は<strong>鍼灸師</strong>をしており、常に相手のニーズを探りながら背景を踏まえてベストな答えを出すことを心がけておりました。<br/>開発も好きですが、やはり人とお話しすることがとても好きです。
+              <strong>様々な人の考えや、経験に触れながら協力することでこそ、学びも発想も人生も！より深まる</strong>と考えているので、ぜひ一緒に頑張りましょう！
               </p>
             </motion.div>
           </motion.div>
@@ -181,19 +158,13 @@ export default function Home() {
                 <h4 className={`${archivoBlack.className} ${styles.skillCardTitle}`}>BACKEND</h4>
               </motion.div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus nobis voluptas dolorem, ut odit fuga sapiente
-                tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati?
+                <strong>■ Ruby</strong><br/>
+                <strong>■ Ruby on Rails</strong><br/>
+                <br/>
+                以下で表示しているポートフォリオ<br/>&quot;Together&quot;は<br/>Ruby on Railsで製作いたしました。
+
+                <br/>
+                今後はJAVAに挑戦していきたい!
               </p>
             </motion.div>
             <motion.div variants={item} className={styles.skillCard}>
@@ -205,19 +176,15 @@ export default function Home() {
                  <h4 className={`${archivoBlack.className} ${styles.skillCardTitle}`}>FRONTEND</h4>
               </motion.div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus nobis voluptas dolorem, ut odit fuga sapiente
-                tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati?
+              <strong>■ JavaScript</strong><br/>
+              <strong>■ REACT</strong> -学習中<br/>
+              <strong>■ Next.js</strong> -学習中<br/>
+              <strong>■ HTML,CSS</strong><br/>
+              <strong>■ Sass(Scss)</strong><br/>
+              <br/>
+              このページはREACTとNext.jsで製作しています。
+              学習に伴い改良を重ねていきます！<br/>
+              &quot;Together&quot;のデザインはJavaScriptとScSSを用いました。
               </p>
             </motion.div>
             <motion.div variants={item} className={styles.skillCard}>
@@ -229,19 +196,10 @@ export default function Home() {
                  <h4 className={`${archivoBlack.className} ${styles.skillCardTitle}`}>INFRA</h4>
               </motion.div>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Necessitatibus nobis voluptas dolorem, ut odit fuga sapiente
-                tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati? Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Necessitatibus nobis voluptas dolorem, ut odit fuga
-                sapiente tenetur, corporis nam itaque laborum doloribus debitis
-                voluptatem nesciunt recusandae accusamus voluptates maxime?
-                Obcaecati?
+              <strong>■ AWS</strong><br/>
+              <br/>
+              基本的なVPC, EC2, IAM, AMI, S3, RDS...<br/>などです。<br/>
+              まだまだインフラ部分の知識は疎いので努力します！
               </p>
             </motion.div>
           </motion.div>
